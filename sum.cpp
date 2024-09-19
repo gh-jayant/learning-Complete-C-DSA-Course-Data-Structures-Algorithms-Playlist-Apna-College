2d46fb1926b2303056597e7663058939ad70c65c
Jayant Raj <150683214+gh-jayant@users.noreply.github.com>
2024-09-20 00:30:22 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n , i ,sum;
+    for(i=1;i++;sum=0);
+    cout<<"enter a number:";
+    cin>>n;
+    while(i<=n){
+        sum+=i;
+        i++;
+    }
+    cout<<"sum = "<<sum;
+
+    return 0;
+}
