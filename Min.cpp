f306a1109c4026d688000972ba76637157ffcb6a
Jayant Raj <150683214+gh-jayant@users.noreply.github.com>
2024-09-20 00:29:33 +0530

    Add files via upload

---
+++ Min.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a , b;
+
+    cout<<"Enter a:"<<endl;
+    cin>>a;
+
+    cout<<"Enter b:"<<endl;
+    cin>>b;
+
+    if(a<b){
+        cout<<"Min = "<<a<<endl;
+    }else{
+        cout<<"Min = "<<b<<endl;
+    }
+    return 0;
+    }
